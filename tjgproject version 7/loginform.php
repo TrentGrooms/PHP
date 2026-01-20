8c3633e2d646f1bed0d97817bef17dba853eb0c0
Trent <trentgrooms3@gmail.com>
2026-01-20 14:33:10 -0600

    updated for unit 7

---
+++ tjgproject version 7/loginform.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Form</title>
+</head>
+<body>
+
+<h2>Login</h2>
+
+<form action="logindisplay.php" method="post">
+    <label>Username:</label><br>
+    <input type="text" name="username" required><br><br>
+
+    <label>Password:</label><br>
+    <input type="password" name="password" required><br><br>
+
+    <input type="submit" name="submit" value="Login">
+    <input type="submit" name="create" value="Create Login">
+</form>
+
+</body>
+</html>
+
+
